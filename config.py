32e5b77ef2ff0801f3be3cccaefb8d1f4086e0cb
lachlanmaclean <lachlanmaclean97@gmail.com>
2018-04-21 16:31:13 +1200

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
-username = "DadJokesBot_"
-password = "A#cjokes$$159"
-client_id = "hwJj3SiddN9TNQ"
-client_secret = "UTGmDAt5trTdawGz4Jx1cFLerZs"
+username = "REDDIT USERNAME"
+password = "REDDIT PASSWORD"
+client_id = "CLIENT ID"
+client_secret = "SECRET KEY"
+
+#Client ID and Secret key is found on Reddit's dev app page when you create an app.
